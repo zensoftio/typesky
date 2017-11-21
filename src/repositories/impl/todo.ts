303e99e8e80971b9b8acf83fe3a09a76fba253a0
Nick Ro <nick.ro@zensoft.io>
2017-11-21 10:35:23 +0600

    move utils into common folder

--- src/repositories/impl/todo.ts
+++ src/repositories/impl/todo.ts
@@ -4,7 +4,7 @@ import QueryBaseRepository from '../base/impl/query'
 import {TodoMapping} from '../../mappings/index'
 import TodoModel from '../../models/todo'
 import {action} from 'mobx'
-import {patch} from '../../common/utils'
+import {patch} from '../../common/utils/common'
 
 @injectable('TodoRepository')
 export class DefaultTodoRepository extends QueryBaseRepository<TodoMapping> implements TodoRepository {
