16fe44c42908a042359423c81876eaf78d127b81
Nick Ro <nick.ro@zensoft.io>
2017-11-24 15:21:04 +0600

    tests

---
+++ src/tests/index.spec.ts
@@ -0,0 +1,46 @@
+import 'mocha'
+import DefaultPostService from '../services/impl/post'
+import {DefaultPostStore} from '../stores/impl/post'
+import DefaultFetcher from '../fetchers/impl/fetch'
+import {instance, mock, verify, when} from 'ts-mockito'
+import Pathes from '../dicts/pathes'
+
+const genResponse = (value: any) => Promise.resolve({json: () => Promise.resolve(value)}) as any
+
+describe('Hello function', () => {
+  
+  it('should return hello world', async () => {
+    
+    const MockedPostStore = mock(DefaultPostStore)
+    const MockedFetcher = mock(DefaultFetcher)
+    const postStore = instance(MockedPostStore)
+    const fetcher = instance(MockedFetcher)
+    
+    when(MockedFetcher.get(Pathes.Post.byId(1)))
+      .thenReturn(genResponse('check'))
+    
+    when(MockedPostStore.prepare('postById'))
+    
+    const postService = new DefaultPostService(postStore)
+    postService.setFetcher(fetcher)
+    
+    await postService.loadPost(1)
+    
+    // check if loadings was setted
+    verify(MockedPostStore.prepare('postById'))
+      .once()
+    
+    // check if we fetch data from server
+    verify(MockedFetcher.get(Pathes.Post.byId(1)))
+      .once()
+    
+    // check if we load data into store
+    verify(MockedPostStore.load('postById', 'check'))
+      .once()
+    
+    // check if loadings was setted before load
+    verify(MockedPostStore.prepare('postById'))
+      .calledBefore(MockedPostStore.load('postById', 'check'))
+  })
+  
+})
