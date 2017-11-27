d7f49b05ce53e17d8a7aa32a8e9db6e71d2f63d9
Nick Ro <nick.ro@zensoft.io>
2017-11-27 15:50:33 +0600

    class loader for system layers

--- src/mappers/index.ts
+++ src/mappers/index.ts
@@ -3,10 +3,6 @@ import PostModel from '../models/post'
 import ResultSet from '../results/base/result'
 import {Maybe} from '../common/types'
 
-// here we need to list all implementations
-export * from './impl/todo'
-export * from './impl/post'
-
 export interface TodoMapper {
   
   all: TodoModel[]
